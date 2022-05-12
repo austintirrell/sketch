92377bde6763648a958c258f8ff22ce483a8a1cd
atirrell <austinjtirrell@gmail.com>
2022-05-11 21:48:07 -0500

    logic for creating grid

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let gridSize = document.getElementById('gridSize');
+const gridSize = document.getElementById('gridSize');
 const gridSizeDisplay = document.getElementById('gridSizeDisplay');
 const grid = document.getElementById('grid');
+const colorPicker = document.getElementById('colorPicker');
+const colorButton = document.getElementById('color');
+const rainbowButton = document.getElementById('rainbow');
+const resetButton = document.getElementById('reset');
 
 window.onload = construct();
 
@@ -9,17 +13,19 @@ gridSize.addEventListener('input', function() {
     construct();
 });
 
+
+
 function construct() {
-    resetGrid();
+    //if pixels exist, erase
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    //build new pixel grid
     for (i=0; i < gridSize.value * gridSize.value; i++) {
         let pixel = document.createElement('div');
         grid.appendChild(pixel);
         grid.lastChild.classList.add('pixel');
     }
-}
-
-function resetGrid() {
-    while (grid.firstChild) {
-        grid.removeChild(grid.firstChild);
-    }
+    grid.style.gridTemplateColumns = 'repeat('+gridSize.value+', 1fr)';
+    grid.style.gridTemplateRows = 'repeat('+gridSize.value+', 1fr)';
 }
